d5a37281c06fee52046116321c793f0320cf2783
ysoolim1121 <ysoolim1121@ajou.ac.kr>
2026-01-13 22:14:44 +0900

    Seplia에 배포하고 코드 검증

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,31 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://ethereum-sepolia-rpc.publicnode.com";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""; // 새로 추가됨!
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20", // 버전 맞춤
+  solidity: "0.8.20",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY !== "" ? [PRIVATE_KEY] : [],
+    },
+  },
+  // 👇 여기가 새로 추가된 부분입니다!
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: false
+  }
 };
 
 export default config;
